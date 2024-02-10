833dc0c10b586098c2cc5c5b77b106318aca6e13
Asparaghost <leimeandyou@gmail.com>
2024-02-10 21:41:51 +0800

    home done

--- cms/views.py
+++ cms/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "cms/index.html")
+
+def projects(request):
+    return render(request, "cms/projects.html")
